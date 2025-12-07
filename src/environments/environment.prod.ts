5973cd7c2d032b98b7f90134cd2674da0ccaaf70
Oriol Alàs Cercós <oriol.alas@eurecat.org>
2025-12-07 12:58:58 +0100

    changed API in prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8000/api' // This should be updated with the production API URL when available
+  apiUrl: 'https://innwater.eurecatprojects.com/repte/api' // This should be updated with the production API URL when available
 };
